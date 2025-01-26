463bde991af3a11d2d2f5ff8600da4fd453a2c24
pawan <pawantamang@Pawans-MacBook-Pro.local>
2025-01-26 19:35:00 +0545

    new admin

--- adminSeed.js
+++ adminSeed.js
@@ -2,11 +2,11 @@ const User = require("./model/userModel")
 const bcrypt = require("bcrypt")
 
 const adminSeed = async() =>{
-    const adminExist = await User.findOne({userEmail : "admin001@gmail.com"})
+    const adminExist = await User.findOne({userEmail : "admin002@gmail.com"})
     if(!adminExist){
       await User.create({
             userName : "admin",
-            userEmail : "admin001@gmail.com",
+            userEmail : "admin002@gmail.com",
             userPhoneNumber :"982389380",
             userPassword : bcrypt.hashSync("adminpassword",10),
             role : "admin"
